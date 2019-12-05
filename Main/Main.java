a25ceaeb62a124ce200530ad4fc1be4df198c405
yamikhsan <yahyamaulana450@gmail.com>
2019-12-05 13:35:45 +0700

    test String[] args

--- Main/Main.java
+++ Main/Main.java
@@ -2,7 +2,16 @@ public class Main{
     public static void main(String[] args){
         // System.out.println("Hello");
 
-        Car bmw = new Car("BMW i3000");
-        bmw.forward();
+        // Car bmw = new Car("BMW i3000");
+        // bmw.forward();
+
+        // Test String[] args
+        if(args.length > 0){
+            for(String a: args){
+                System.out.println(a);
+            }
+        } else {
+            System.out.print("args tidak boleh kosong");
+        }
     }
 }
