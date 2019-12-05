0ef4063b8a5211394a43a5c9110dbe3ba0b0b5bb
yamikhsan <yahyamaulana450@gmail.com>
2019-12-05 18:10:59 +0700

    add try-catch

--- Main/Tambah.java
+++ Main/Tambah.java
@@ -1,11 +1,15 @@
 public class Tambah{
     public static void main(String[] args){
         if(args.length == 2){
-
-            int num1 = Integer.parseInt(args[0]);
-            int num2 = Integer.parseInt(args[1]);
-            int res = num1 + num2;
-            System.out.print(res);
+            try {
+                int num1 = Integer.parseInt(args[0]);
+                int num2 = Integer.parseInt(args[1]);
+                int res = num1 + num2;
+                System.out.print(res);
+                
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
             
         }else{
             System.out.print("args harus 2");
