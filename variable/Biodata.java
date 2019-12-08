7a3887de9ff60a54f04624805a5cb8bf0f140d58
yamikhsan <yahyamaulana450@gmail.com>
2019-12-08 13:17:42 +0700

    add variable/Biodata.java

---
+++ variable/Biodata.java
@@ -0,0 +1,19 @@
+public class Biodata{
+    public static void main(String[] agrs){
+        
+        final String nama = "Yahya M I";
+        final String tempat = "Sragen";
+        final int tanggal = 13;
+        final int bulan = 05;
+        final int tahun = 1995;
+        double umur = 24.50;
+        String alamat = "JL KH Mas Mansyur Gg Mede kel. Sudimara Pinang kec. Pinang, Kota Tangerang Banten";
+
+        System.out.println("Nama = " + nama);
+        System.out.println("TTL = " + tempat + ", " + tanggal + "-" + bulan + "-" + tahun);
+        System.out.println("Umur = " + umur);
+        System.out.println("Alamat = " + alamat);
+
+    }
+
+}
