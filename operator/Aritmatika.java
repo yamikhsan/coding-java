07acd0e8013b6c92f590bcd4fd14a36d734877b3
yamikhsan <yahyamaulana450@gmail.com>
2019-12-09 12:47:39 +0700

    add operator

---
+++ operator/Aritmatika.java
@@ -0,0 +1,17 @@
+public class Aritmatika{
+    public static void main(String[] args){
+        int a = 7;
+        int b = 5;
+        System.out.println("Tambah = " + (a+b));
+        System.out.println("Kurang = " + (a-b));
+        System.out.println("Kali = " + (a*b));
+        System.out.println("Bagi = " + (a/b));
+        System.out.println("Modular = " + (a%b));
+        System.out.println("Increment a = " + (++a));
+        System.out.println("Increment b = " + (++b));
+        System.out.println("Decrement a = " + (--a));
+        System.out.println("Decrement b = " + (--b));
+        // System.out.println(a++);
+        // System.out.println(a);
+    }
+}
---
+++ operator/Perbandingan.java
@@ -0,0 +1,20 @@
+public class Perbandingan{
+    public static void main(String[] args){
+
+        int a = 7;
+        int b = 5;
+
+        String i = "i";
+        String j = "j";
+
+        System.out.println("a < b = " + (a<b));
+        System.out.println("a > b = " + (a>b));
+        System.out.println("a >= b = " + (a>=b));
+        System.out.println("a <= b = " + (a<=b));
+        System.out.println("a == b = " + (a==b));
+        System.out.println("a != b = " + (a!=b));
+        System.out.println("i == j = " + (i==j));
+        System.out.println("i != j = " + (i!=j));
+
+    }
+}
